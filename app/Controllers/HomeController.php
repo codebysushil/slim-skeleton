0659c4c44a8093dd836ffa6b2c7333057b8940a5
Sushil Kumar <sushilkumbhar77@gmail.com>
2026-05-28 07:16:41 +0530

    update and style fix

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Slim\App\Controllers;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 
-class HomeController extends BaseController
+final class HomeController extends BaseController
 {
     public function index(Request $request, Response $response, array $args): Response
     {
--- routes/route.php
+++ routes/route.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Slim\App\Controllers\HomeController;
 
 $app->get('/', [HomeController::class, 'index']);
