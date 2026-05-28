623c3f7a67a44f26f68420326fccc169644e5583
Sushil Kumar <sushilkumbhar77@gmail.com>
2026-05-28 16:14:45 +0530

    phpunit code coverage fix

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -4,13 +4,27 @@ declare(strict_types=1);
 
 namespace Slim\App\Config;
 
-class Database
+use Throwable;
+
+final class Database
 {
     public $db;
 
-    try {
-        $this->db = new PDO('sqlite:database.sqlite3');
-    } catch (\Throwable $th) {
-        //throw $th;
+    private string $host = '127.0.0.1';
+
+    private string $username = 'root';
+
+    private string $password = '';
+
+    private string $database = 'slim';
+
+    public function __construct()
+    {
+        $this->db = null;
+        try {
+            $this->db = new mysqli(hostname: $this->host, username: $this->username, password: $this->password, database: $this->database);
+        } catch (Throwable $th) {
+            throw $th;
+        }
     }
 }
